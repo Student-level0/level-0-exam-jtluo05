9171e545e9576c7fedf7ec62e448783632b45860
plooba <league@WTS13.attlocal.net>
2017-11-21 19:53:38 -0800

    we did the level zero test

--- src/CodingExercise1.java
+++ src/CodingExercise1.java
@@ -5,15 +5,33 @@ import org.jointheleague.graphical.robot.Robot;
 /* Level 0 Exam: Coding  Exercise #1 */
 public class CodingExercise1 {
 
-    public static void main(String[] args) {
-   	 // 3. ask the user what color they would like the Robot to draw
-
-   	 // 4. use an if/else statement to set the pen color that the user requested (minimum of 2 colors)
-
-   	 // 2. set the pen width to 10
-
-   	 // 1. make the Robot draw a shape
-   	 
-    }
+	public static void main(String[] args) {
+		Robot bob = new Robot();
+		// 3. ask the user what color they would like the Robot to draw
+		String color = JOptionPane.showInputDialog("What color would you like me to draw a square in? Green or Red?");
+		// 4. use an if/else statement to set the pen color that the user requested
+		// (minimum of 2 colors)
+		if (color.equalsIgnoreCase("red")) {
+			bob.setPenColor(255, 44, 44);
+		} else if (color.equalsIgnoreCase("green")) {
+			bob.setPenColor(44, 255, 44);
+		} else {
+			JOptionPane.showMessageDialog(null, "ERROR!!!!!YOU DID NOT CHOOSE ONE OF THE OPTIONS!!!!! "
+					+ "I will pick the color for you. I will draw it in... green.");
+			bob.setPenColor(44, 255, 44);
+		}
+		// 2. set the pen width to 10
+		bob.setPenWidth(10);
+		// 1. make the Robot draw a shape
+		bob.penDown();
+		bob.setSpeed(1000);
+		bob.move(100);
+		bob.turn(90);
+		bob.move(100);
+		bob.turn(90);
+		bob.move(100);
+		bob.turn(90);
+		bob.move(100);
+	}
 
 }
--- src/CodingExercise2.java
+++ src/CodingExercise2.java
@@ -1,17 +1,32 @@
 
-
 /* Level 0 Exam: Coding  Exercise #2 */
 import javax.swing.JOptionPane;
 
 /**
- * Write a program that asks the user for their age. 
- * Tell them which year they were born. 
- * If they are over 30, tell them they are too old to play this game.
+ * Write a program that asks the user for their age. Tell them which year they
+ * were born. If they are over 30, tell them they are too old to play this game.
  **/
 
 public class CodingExercise2 {
+	public static void main(String[] args) {
+		String age1 = JOptionPane.showInputDialog("How old are you?");
+		int age = Integer.parseInt(age1);
+		if (age > 31) {
+			JOptionPane.showMessageDialog(null, "Sorry, you are too old to play this amazing game.");
 
+		}
+	
+		else {
+				JOptionPane.showMessageDialog(null, "You were born in " + (2017-age));
+		}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
-
-
-
